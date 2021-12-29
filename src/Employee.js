a3d4e7788f4007b9adcebb90324c1db32b909711
BishtGaurav0 <bisht1999gaurav@gmail.com>
2021-12-29 18:15:02 +0530

    second react

---
+++ src/Employee.js
@@ -0,0 +1,51 @@
+import React, { useState} from "react";
+import "./Employee.css";
+
+function Employee() {
+  const[name,setName]=useState("");
+  const[dept,setDept]=useState("");
+  const[rank,setRank]=useState("");
+  const[list,setList]=useState([]);
+
+  const Submit=(e)=>{
+    e.preventDefault();
+    const data={name,dept,rank}
+    if(name&&dept&&rank){
+      setList((lists)=>[...lists,data])
+      setName("")
+      setDept("")
+      setRank("")
+    }
+  }
+
+  return (
+    <div className="employee">
+            <div className="heading">EMPLOYEE FEEDBACK FORM</div>
+            <form action="" onSubmit={Submit}>
+                <div>
+                    <label htmlFor='name' className='title'>Name:</label>
+                    <input type="text" name='name' id='name' className='Border' value={name} onChange={(e)=>setName(e.target.value)}/><br></br>
+                    <label htmlFor='dept' className='title'>Department:</label>
+                    <input type="text" name='dept' id='dept' className='Border' value={dept} onChange={(e)=>setDept(e.target.value)}/><br></br>
+                    <label htmlFor='rating' className='title'>Rating:</label>
+                    <input type="number" name='rating' id='rating' className='Border' value={rank} onChange={(e)=>setRank(e.target.value)}/><br></br>
+                </div>
+                <button type='submit' className='button'>Submit</button> 
+            </form>
+        <div className="Display">
+          
+          {
+            list.map((a)=>
+            <div className="record">
+            
+            <h2>Name: {a.name} | Department: {a.dept} | Rating: {a.rank}</h2>
+            
+            </div>)
+          }
+          </div>
+      </div>
+    
+  );
+}
+
+export default Employee;
